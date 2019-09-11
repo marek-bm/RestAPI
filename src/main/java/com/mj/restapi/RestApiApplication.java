b51684d2f5adec3aca94379ab833245c9f6164aa
marek <marek.jastrzebski@outlook.com>
2019-09-11 19:07:28 +0200

    removed unused imports

--- src/main/java/com/mj/restapi/RestApiApplication.java
+++ src/main/java/com/mj/restapi/RestApiApplication.java
@@ -1,11 +1,7 @@
 package com.mj.restapi;
 
-import com.mj.restapi.dao.ItemDao;
-import com.mj.restapi.dao.ItemDaoImpl;
-import com.mj.restapi.model.Item;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
